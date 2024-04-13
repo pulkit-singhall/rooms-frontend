37a2caad0a8a554ebbd484f130029dfa1198581a
pulkit-singhall <singhal.pulkit667@gmail.com>
2024-04-14 01:12:36 +0530

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Home from './screens/home';
+import Room from './screens/room';
+import Error from './screens/error'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <Error/>
+  },
+  {
+    path: "/room",
+    element: <Room />,
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
---
+++ src/screens/home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+    return (
+        <>
+            <h1 className="text-red-500">home</h1>
+        </>
+    );
+}
+
+export default Home;
